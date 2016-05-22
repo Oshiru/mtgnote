dda8ab912a7522e3d64bedaba830b858f419d101
Michael Boyd <mboyd855@gmail.com>
2016-05-22 19:15:10 +0100

    Initial commit of setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mtgnote',
+      version='0.1',
+      description='Helps to create annotated collections of mtg cards',
+      url='http://github.com/Oshiru/mtgnote',
+      author='Michael Boyd',
+      author_email='mboyd855@gmail.com',
+      license='MIT',
+      packages=['mtgnote'],
+      zip_safe=False)
